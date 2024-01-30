7f87ade842592d4406b4b171eaf029ff64814c6c
dipakp-logicrays <dipakp@logicrays.com>
2024-01-30 18:57:13 +0530

    Add tests for Box class

--- Test/Unit/BoxTest.php
+++ Test/Unit/BoxTest.php
@@ -9,4 +9,28 @@ class BoxTest extends TestCase
         $this->assertTrue($box->has('toy'));
         $this->assertFalse($box->has('ball'));
     }
+
+    public function testTakeOneFromTheBox()
+    {
+        $box = new App\Box(['torch']);
+        $this->assertEquals('torch', $box->takeOne());
+
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
+    }
+
+    public function testStartsWithALetter()
+    {
+        $box = new App\Box(['toy', 'torch', 'ball', 'cat', 'tissue']);
+
+        $results = $box->startsWith('t');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('toy', $results);
+        $this->assertContains('torch', $results);
+        $this->assertContains('tissue', $results);
+
+        // Empty array if passed even
+        $this->assertEmpty($box->startsWith('s'));
+    }
 }
